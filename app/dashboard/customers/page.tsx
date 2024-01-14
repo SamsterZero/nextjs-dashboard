a54be831ed97a19cb584e3d1899233196473d4a7
Vinay Mhatre <90505086+SamsterZero@users.noreply.github.com>
2024-01-14 18:16:49 +0530

    Authentication
    Added authentication & configured metadata to change the information in the head

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,16 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+import { lusitana } from '@/app/ui/fonts';
 export default function Page() {
-    return <p>Customers Page</p>;
-  }
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+    </div>
+  );
+}
